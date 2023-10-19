0239eb0dab9129c7fe05d6924c3da6584de8a21b
Emilse Arias <emilsearias@gmail.com>
2023-10-19 17:55:43 -0300

    agregamos variables, mensajes por consola y pantalla

--- js/1-variables.js
+++ js/1-variables.js
@@ -8,3 +8,30 @@ lineas
 
 // declarar una variable (var - let - const)
 let anioActual = 2023;
+const url = "https://universe.rollingcodeschool.com/login";
+
+//mostrar mensajes 
+console.log('Este es un mensaje en consola');
+console.log(anioActual);
+console.log(url);
+console.log('Año: '+ (anioActual + 2));
+
+// modificar el contenido de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+// url='otro valor';
+console.log(url);
+
+document.write('<h1>Prueba de <br>titulo</h1>');
+document.write('Año: '+anioActual + '<br>');
+document.write('Año: '+anioActual);
+
+// ventanas emergentes
+alert('Esto es una prueba de alerta');
+
+let color = prompt('Ingrese un color');
+console.log(color);
+
+
+
