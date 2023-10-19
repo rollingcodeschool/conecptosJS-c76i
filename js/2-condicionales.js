9ecb5c19f6d25b7136a7c175b0f6ac7388ad09b1
Emilse Arias <emilsearias@gmail.com>
2023-10-19 18:37:48 -0300

    agregamos un ejemplo de una condicional simple

---
+++ js/2-condicionales.js
@@ -0,0 +1,13 @@
+/*  if(condicion logica){
+     codigo a ejecutar si se cumple la condicion logica
+ }
+*/
+
+// 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+let numero1 = parseInt(prompt('Ingrese un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+
+if( numero1 > numero2){
+    console.log('La variable numero 1 es mayor la variable 2')
+}
