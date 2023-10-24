1ccbce1c4f8ab1710f1a98c8f0b0fd7f28e65b98
Emilse Arias <emilsearias@gmail.com>
2023-10-24 17:11:08 -0300

    agregamos un else if en el ejercicio 6

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -5,15 +5,14 @@
 // 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
 // 6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 
-const numero1 = parseInt(prompt('Ingrese un primer numero'));
-const numero2 = parseInt(prompt('Ingrese un segundo numero'));
+const numero1 = parseInt(prompt("Ingrese un primer numero"));
+const numero2 = parseInt(prompt("Ingrese un segundo numero"));
 
-if( numero1 > numero2){
-    console.log('El numero '+numero1+' es mayor que el numero '+numero2);
-} else{
-    if(numero2 > numero1){
-        console.log('El numero '+numero2+' es mayor que el numero '+numero1);
-    }else{
-        console.log('Los numeros ingresados son iguales ('+numero2+')');
-    }
+if (numero1 > numero2) {
+  document.write("El numero " + numero1 + " es mayor que el numero " + numero2);
+} else if (numero2 > numero1) {
+//   document.write("El numero " + numero2 + " es mayor que el numero " + numero1);
+  document.write(`El número ${numero2} es mayor que el número ${numero1}`);
+} else {
+  document.write("Los numeros ingresados son iguales (" + numero2 + ")");
 }
