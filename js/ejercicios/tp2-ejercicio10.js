5962e9595336e68fcea8d50d1694c0c9a2709916
Emilse Arias <emilsearias@gmail.com>
2023-10-26 18:39:09 -0300

    agregamos los valores de las celdas en el ejercicio 10 del tp 2

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -3,13 +3,14 @@
 const filas = parseInt(prompt("Ingrese un número de filas"));
 const columnas = parseInt(prompt("Ingrese un número de columnas"));
 
-const totalCeldas = filas * columnas;
+let totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
     document.write(`<tr>`);
     for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
-        document.write(`<td>35</td>`);
+        document.write(`<td>${totalCeldas}</td>`);
+        totalCeldas--; // totalCeldas = totalCeldas - 1
     }
     document.write(`</tr>`);
 }
