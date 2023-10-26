c3db32c5608397b608bbf4334fbf3a4eb501e14d
Emilse Arias <emilsearias@gmail.com>
2023-10-26 18:26:48 -0300

    dibujamos la cantidad de columnas indicada por el usuario

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -7,11 +7,10 @@ const totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
-    document.write(`
-    <tr>
-            <td>35</td>
-            <td>34</td>
-            <td>33</td>
-    </tr>`);
+    document.write(`<tr>`);
+    for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
+        document.write(`<td>35</td>`);
+    }
+    document.write(`</tr>`);
 }
 document.write(`</tbody></table>`);
