28d94917215a6ebae080f4e677e19d280c77bfa3
Emilse Arias <emilsearias@gmail.com>
2023-10-26 16:57:38 -0300

    agregamos el bucle del ejercicio 09 del tp 1

--- js/ejercicios/tp1-ejercicio9.js
+++ js/ejercicios/tp1-ejercicio9.js
@@ -4,9 +4,12 @@
 let frase = prompt('Ingrese una frase');
 console.log(frase.length);
 console.log(frase.charAt(0));
+frase = frase.toLowerCase(); 
 
 // hola mundo
 // Escribe un programa
-if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e' || frase.charAt(0) === 'i' || frase.charAt(0) === 'o'|| frase.charAt(0) === 'u'){
-    document.write(frase.charAt(0))
+for(let i =0; i < frase.length; i++){
+    if(frase.charAt(i) === 'a' || frase.charAt(i) === 'e' || frase.charAt(i) === 'i' || frase.charAt(i) === 'o'|| frase.charAt(i) === 'u'){
+        document.write(frase.charAt(i))
+    }
 }
