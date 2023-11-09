b7614e5a3c04cca302e4140c709ab2022f1bd377
Emilse Arias <emilsearias@gmail.com>
2023-11-09 18:56:31 -0300

    agregamos un objeto con notación literal

---
+++ js/7-objetos.js
@@ -0,0 +1,38 @@
+//notacion literal
+
+const pelicula = {
+    //propiedades -> clave : valor
+    titulo: 'La mascara',
+    anio : 1994,
+    duracion: '1hr 40 min',
+    genero: ['comedia', 'fantasia'],
+    //metodos
+    iniciarPeli: function (){
+        document.write(`<p>...Comenzando la reproducción</p>`)
+    },
+    pausa: () =>{
+        document.write(`<p>Se pauso la pelicula</p>`)
+    }
+}
+
+//mostrar un objeto
+console.log(pelicula);
+document.write(pelicula);
+document.write(`<h1>Pelicula: ${pelicula.titulo}</h1>`)
+document.write(`<p>Duración: ${pelicula.duracion}</p>`)
+document.write(`<p>Año de estreno: ${pelicula['anio']}</p>`)
+document.write(`<p>Duración: ${pelicula.duracio}</p>`)
+
+//agregar una nueva propiedad a un objeto
+pelicula.estado = true;
+
+console.log(pelicula);
+
+//modificar una propiedad del objeto
+pelicula.estado = false;
+
+console.log(pelicula);
+
+//agregamos un nuevo genero
+pelicula.genero.push('Aventura');
+console.log(pelicula.genero)
